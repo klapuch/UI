d9d224fde1a0f55c7e0e88465f436dd6ef84f111
Dominik Klapuch <klapuchdominik@gmail.com>
2017-10-09 22:37:09 +0200

    Better fake

--- Core/FakePagination.php
+++ Core/FakePagination.php
@@ -8,10 +8,17 @@ use Klapuch\Output;
  * Fake
  */
 final class FakePagination implements Pagination {
+	private $range;
+
+	public function __construct(array $range = null) {
+		$this->range = $range;
+	}
+
 	public function print(Output\Format $format): Output\Format {
 		return $format;
 	}
 
 	public function range(): array {
+		return $this->range;
 	}
 }
